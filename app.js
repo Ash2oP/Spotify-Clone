cec0fba21b7419ed87404f7f7d60d1f435f134e8
Ash2oP <aashishrajsingh9468@gmail.com>
2025-05-28 10:26:22 +0000

    playlists working+back btn added

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 let playlistBtn = document.querySelectorAll(".playlist-button");
 let playlistContainer = document.querySelector(".playlist-container");
+let backBtn = document.querySelector("#Back-btn");
+let homeBtn = document.querySelector("#Home-btn");
 let isPlaylistOpen = false;
 
 for(let btn of playlistBtn) {
-    btn.addEventListener("click", ()=> {
-        console.log("We r in");
-        if(isPlaylistOpen){
-            playlistContainer.classList.add("hide");
-        } else {
-            playlistContainer.classList.remove("hide");
-        }
+    btn.addEventListener("click", () => {
+        window.history.pushState({ view: "playlist"}, "", "#playlist");
+        playlistContainer.classList.remove("hide");
+        isPlaylistOpen = true;
     });
 }
+
+homeBtn.addEventListener("click", () => {
+    window.history.pushState({view : "home"}, "", "#home");
+});
+
+backBtn.addEventListener("click", () => {
+    window.history.back();
+});
+
+window.addEventListener("popstate", (data) => {
+    if( !data.state || data.state.view !== "playlist"){
+        playlistContainer.classList.add("hide");
+        isPlaylistOpen = false;
+    }
+});
