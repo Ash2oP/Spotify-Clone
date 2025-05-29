11c2ded09444d3ecd77d3c9121b52d925bd80c32
Ash2oP <aashishrajsingh9468@gmail.com>
2025-05-29 11:53:22 +0000

    added working topbar menu

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const playlistEle = {
     playlistPfp : document.querySelectorAll(".card-image"),
     playlistPfpContainer : document.querySelector(".playlist-header-pfp"),
     playlistTitle : document.querySelectorAll(".card-disc"),
-    playlistTitleContainer : document.querySelector(".playlist-header-title")
+    playlistTitleContainer : document.querySelector(".playlist-header-title"),
+    playlistMenuIcon : document.querySelector("#topbar-menu-icon"),
+    playlistTopbarMenu : document.querySelector(".topbar-menu-dropdown")
 };
 let isPlaylistOpen = false;
+let isPlaylistMenuOpen = false;
 let backBtn = document.querySelector("#Back-btn");
 let homeBtn = document.querySelector("#Home-btn");
+
 
 // Hiding Playlist
 const hidePlaylist = () => {
@@ -32,6 +36,18 @@ const fillPLaylist = (idx) => {
     playlistEle.playlistTitleContainer.innerHTML = playlistEle.playlistTitle[idx].innerHTML;
 }
 
+// Hiding Playlist Topbar Menu
+const hidePLaylistTopbarMenu = () => {
+    playlistEle.playlistTopbarMenu.classList.add("hide");
+    isPlaylistMenuOpen = false;
+}
+
+// Activate Playlist Topbar Menu
+const activatePLaylistTopbarMenu = () => {
+    playlistEle.playlistTopbarMenu.classList.remove("hide");
+    isPlaylistMenuOpen = true;
+}
+
 playlistEle.playlistBtn.forEach((btn, idx) => {
     btn.addEventListener("click", () => {
         window.history.pushState({ view: "playlist"}, "", "#playlist");
@@ -43,6 +59,7 @@ playlistEle.playlistBtn.forEach((btn, idx) => {
 homeBtn.addEventListener("click", () => {
     window.history.pushState({view : "home"}, "", "#home");
     hidePlaylist();
+    hidePLaylistTopbarMenu();
 });
 
 backBtn.addEventListener("click", () => {
@@ -52,8 +69,18 @@ backBtn.addEventListener("click", () => {
 window.addEventListener("popstate", (data) => {
     if( !data.state || data.state.view !== "playlist"){
         hidePlaylist();
+        hidePLaylistTopbarMenu();
     }
     if( data.state.view === "playlist"){
         activatePlaylist();
+    }
+});
+
+playlistEle.playlistMenuIcon.addEventListener("click", () => {
+    if(isPlaylistMenuOpen){
+        hidePLaylistTopbarMenu();
+    }
+    else {
+        activatePLaylistTopbarMenu();
     }
 });
